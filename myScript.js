7074e6b16bcd54d4314d205b8c5ec1a237a31725
CarlaDCyr <118022663+CarlaDCyr@users.noreply.github.com>
2022-12-25 19:49:26 -0700

    added random button and javascript

---
+++ myScript.js
@@ -0,0 +1,9 @@
+// Connecting HTML to JS
+const drinkName = document.getElementById("drinkName")
+const drinkImage = document.getElementById("drinkImage")
+const getRandom = document.getElementById("randomButton")
+const ingredientList = document.getElementById("cocktailIngredients")
+const category = document.getElementById("findByCategory")
+const glass = document.getElementById("findByGlass")
+const ingredient = document.getElementById("findByIngredient")
+
